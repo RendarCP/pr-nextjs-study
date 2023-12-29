d138dc9eb5204cdcfa63399a114c47975d7ceb02
Cho Seong week <tjddnr0217@gmail.com>
2023-12-29 23:04:00 +0900

    feat: 정적 페이지 제작 전용 푸시

--- src/app/products/layout.tsx
+++ src/app/products/layout.tsx
@@ -1,5 +1,11 @@
+import { Metadata } from "next";
 import Link from "next/link";
 import styles from "./layout.module.css";
+
+export const metadata: Metadata = {
+  title: "멋진 제품 사이트 | 전체 제품 확인",
+  description: "멋진 제품을 확인해 보세요",
+};
 
 export default function ProductsLayout({
   children,
@@ -9,8 +15,8 @@ export default function ProductsLayout({
   return (
     <>
       <nav className={styles.nav}>
-        <Link href="/products/woman">여성</Link>
-        <Link href="/products/man">남성</Link>
+        <Link href="/products/women">여성옷</Link>
+        <Link href="/products/man">남성옷</Link>
       </nav>
       <section className={styles.product}>{children}</section>
     </>
