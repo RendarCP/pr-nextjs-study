9bcb311ea32b5a78a8a689ef8d443de9fedff8e5
Cho Seong week <tjddnr0217@gmail.com>
2024-01-07 20:19:21 +0900

    feat: Font관련된 부분 푸시

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,22 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Metadata } from "next";
+import Link from "next/link";
 import "./globals.css";
 import styles from "./layout.module.css";
-import Link from "next/link";
+import { Open_Sans } from "next/font/google";
+import { Nanum_Gothic } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
+const sans = Open_Sans({ subsets: ["latin"] });
+const gothic = Nanum_Gothic({
+  weight: "700",
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
   title: "멋진 제품 사이트",
   description: "멋진 제품을 판매하는 곳입니다.",
+  icons: {
+    icon: "/favicon.ico",
+  },
 };
 
 export default function RootLayout({
@@ -17,14 +25,19 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
+    <html lang="en" className={sans.className}>
+      {/*
+        <head /> will contain the components returned by the nearest parent
+        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
+      */}
+      <head />
+      <body>
         <header className={styles.header}>
-          <h1>Demo Note</h1>
+          <h1 className={gothic.className}>Demo Note</h1>
           <nav className={styles.nav}>
             <Link href="/products">Products</Link>
-            <Link href="/contact">Conatact</Link>
             <Link href="/about">About</Link>
+            <Link href="/contact">Contact</Link>
           </nav>
         </header>
         {children}
