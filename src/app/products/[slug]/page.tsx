6894f317946e84e1b2585ab15c6da0a2908c5ef9
Cho Seong week <tjddnr0217@gmail.com>
2024-01-07 20:20:59 +0900

    Feat : 로딩 및 에러 UI 푸시

--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -1,5 +1,7 @@
-import { getProduct, getProducts } from "@/service/products";
-import { notFound } from "next/navigation";
+import GoProductsButton from '@/components/GoProductsButton';
+import { getProduct, getProducts } from '@/service/products';
+import Image from 'next/image';
+import { notFound, redirect } from 'next/navigation';
 
 export const revalidate = 3;
 
@@ -19,11 +21,22 @@ export default async function ProductPage({ params: { slug } }: Props) {
   const product = await getProduct(slug);
 
   if (!product) {
-    notFound();
+    redirect('/products');
+    // notFound();
   }
 
-  // 서버 파일에 있는 데이터중 해당 제품의 정보를 찾아서 그걸 보여줌
-  return <h1>{product.name} 제품 설명 페이지</h1>;
+  return (
+    <>
+      <h1>{product.name} 제품 설명 페이지</h1>
+      <Image
+        src={`/images/${product.image}`}
+        alt={product.name}
+        width='300'
+        height='300'
+      />
+      <GoProductsButton />
+    </>
+  );
 }
 
 export async function generateStaticParams() {
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,21 +1,26 @@
-import { getProducts } from "@/service/products";
-import Link from "next/link";
+import MeowArticle from '@/components/MeowArticle';
+import { getProducts } from '@/service/products';
+import Image from 'next/image';
+import Link from 'next/link';
+import clothesImage from '../../../public/images/clothes.jpg';
 
-export const revalidate = 3;
+// export const revalidate = 3;
 
 export default async function ProductsPage() {
-  // 서버 파일(데이터베이스)에 있는 제품의 리스트를 읽어와서, 그걸 보여줌
   const products = await getProducts();
+
   return (
     <>
       <h1>제품 소개 페이지!</h1>
+      <Image src={clothesImage} alt='Clothes' priority />
       <ul>
-        {products.map((product, index) => (
+        {products.map(({ id, name }, index) => (
           <li key={index}>
-            <Link href={`/products/${product.id}`}>{product.name}</Link>
+            <Link href={`/products/${id}`}>{name}</Link>
           </li>
         ))}
       </ul>
+      <MeowArticle />
     </>
   );
 }
